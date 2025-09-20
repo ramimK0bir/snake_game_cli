a17707faf6a809fc7de96f52e08d65ba6251873f
ramimK0bir <ramim1000r@gmail.com>
2025-09-20 20:34:07 +0600

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -39,7 +39,7 @@ def score_bar(score):
 
 game_over = f"""
 ----------------------
-|  Game Over          |
+|  Game Over         |
 |{ f"score :{score}".center(20) }|
 ----------------------
 """
@@ -140,6 +140,7 @@ if __name__ == "__main__":
         asyncio.run(main())
     except KeyboardInterrupt:
         print("Program stopped.")
+
 
 
 
